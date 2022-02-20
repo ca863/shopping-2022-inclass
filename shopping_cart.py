70bd7a20e756dd8e0d8895ba918b0ad841fb3217
ca863 <ca863@georgetown.edu>
2022-02-20 14:42:47 -0500

    Assembling total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,6 +41,8 @@ def to_usd(my_price):
 
 ## Information Capture / Input
 
+total_price = 0
+
 while True:
     selected_id = input("Please input a product indentifier:")
     if selected_id == "DONE":
@@ -48,11 +50,12 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_products["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 ## Information Display / Output
 
-
+print("TOTAL PRICE" + str(total_price))
 
 # Checkpoint 2: Look-up Products
 # Checkpoint 3: Printing the Receipt
