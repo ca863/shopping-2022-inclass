491a987e1760560ebeb3ee8fd55dd7a499b7a1ba
ca863 <ca863@georgetown.edu>
2022-02-20 14:49:38 -0500

    print receipt at end

--- shopping_cart.py
+++ shopping_cart.py
@@ -42,18 +42,21 @@ def to_usd(my_price):
 ## Information Capture / Input
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product indentifier:")
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_products["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 
 ## Information Display / Output
+For selected_id in selected_ids
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_products["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 print("TOTAL PRICE" + str(total_price))
 
